dacc38c8b5c16d7b41716161eaf5ba9789766528
Brandon Tran <brandonleetran@icloud.com>
2025-05-13 23:34:35 -0500

    chore: v0.0.1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -77,7 +77,7 @@ function Header({ onOpen, theme, setTheme, isDark }: HeaderProps) {
             </a>
           </div>
           <div>
-            <a href="https://github.com/brandonleetran/LeetMonkey/blob/master/CHANGELOG.md" aria-label="v1.0.0-beta.1" title="v1.0.0-beta.1">
+            <a href="https://github.com/brandonleetran/LeetMonkey/blob/master/CHANGELOG.md" aria-label="v1.0.0-beta.1" title="v0.0.1">
               <span className="backdrop-blur-xl hover:bg-white/10 light:hover:bg-black/10 bg-white/5 light:bg-black/5 light:text-[#0d1117] light:border-black/15 light:hover:border-black/50 hover:border-white/50 text-white cursor-pointer border border-white/15 text-xs px-3 py-1 rounded-full transition-all duration-300 ease-in-out">
                 v0.0.1
               </span>
